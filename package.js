8a375bdc7035dbf0969a1bea7b71c02b9f5d423a
Christian Grlica <christian@heavenseven.net>
2016-01-05 17:27:35 +0100

    semantic-ui components

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'heaven7:wsl-theme-semantic-ui',
-  version: '0.0.3_1',
+  version: '0.0.3_2',
   summary: 'Theme semantic ui package',
   git: 'https://github.com/heaven7/wsl-theme-semantic-ui.git',
   documentation: 'README.md'
@@ -8,12 +8,12 @@ Package.describe({
 
 var both = ['client','server'],
     packages = [
-    'heaven7:wsl-core@0.0.3',
-    'heaven7:wsl-i18n@0.0.3',
+    'heaven7:wsl-core@0.0.3_1',
     'semantic:ui-css@2.1.2',
     'semantic:ui@2.1.6',
     'semantic:ui-data@2.1.6',
     'fabienb4:autoform-semantic-ui@0.9.1',
+    'fabienb4:semantic-ui-components@0.5.0',
     'flemay:less-autoprefixer@1.2.0',
     'jquery'
 ];
